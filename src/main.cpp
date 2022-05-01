c7f5335658afb9c864a30ac0ec2485a9652584df
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2022-05-01 15:30:59 +1000

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -26,13 +26,8 @@
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-//#define IMGUI_UNLIMITED_FRAME_RATE
-#ifdef _DEBUG
-#define IMGUI_VULKAN_DEBUG_REPORT
-#endif
-
 static VkAllocationCallbacks* g_Allocator = NULL;
-static VkInstance g_Instance = VK_NULL_HANDLE;
+static vk::Instance g_Instance = VK_NULL_HANDLE;
 static vk::PhysicalDevice g_PhysicalDevice = VK_NULL_HANDLE;
 static vk::Device g_Device = VK_NULL_HANDLE;
 static uint32_t g_QueueFamily = (uint32_t)-1;
@@ -54,122 +49,67 @@ static void check_vk_result(VkResult err)
 		abort();
 }
 
-#ifdef IMGUI_VULKAN_DEBUG_REPORT
-static VKAPI_ATTR VkBool32 VKAPI_CALL debug_report(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType, uint64_t object, size_t location, int32_t messageCode, const char* pLayerPrefix, const char* pMessage, void* pUserData)
-{
-	(void)flags; (void)object; (void)location; (void)messageCode; (void)pUserData; (void)pLayerPrefix; // Unused arguments
-	fprintf(stderr, "[vulkan] Debug report from ObjectType: %i\nMessage: %s\n\n", objectType, pMessage);
-	return VK_FALSE;
-}
-#endif // IMGUI_VULKAN_DEBUG_REPORT
-
 static void SetupVulkan(const char** extensions, uint32_t extensions_count)
 {
 	VkResult err;
 
-	// Create Vulkan Instance
-	{
-		VkInstanceCreateInfo create_info = {};
-		create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-		create_info.enabledExtensionCount = extensions_count;
-		create_info.ppEnabledExtensionNames = extensions;
-#ifdef IMGUI_VULKAN_DEBUG_REPORT
-		// Enabling validation layers
-		const char* layers[] = { "VK_LAYER_KHRONOS_validation" };
-		create_info.enabledLayerCount = 1;
-		create_info.ppEnabledLayerNames = layers;
+	auto layers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+		
+	auto instance_create_info = vk::InstanceCreateInfo()
+		.setEnabledExtensionCount(extensions_count)
+		.setPpEnabledExtensionNames(extensions)
+		.setPEnabledLayerNames(layers);
 
-		// Enable debug report extension (we need additional storage, so we duplicate the user array to add our new extension to it)
-		const char** extensions_ext = (const char**)malloc(sizeof(const char*) * (extensions_count + 1));
-		memcpy(extensions_ext, extensions, extensions_count * sizeof(const char*));
-		extensions_ext[extensions_count] = "VK_EXT_debug_report";
-		create_info.enabledExtensionCount = extensions_count + 1;
-		create_info.ppEnabledExtensionNames = extensions_ext;
-
-		// Create Vulkan Instance
-		err = vkCreateInstance(&create_info, g_Allocator, &g_Instance);
-		check_vk_result(err);
-		free(extensions_ext);
-
-		// Get the function pointer (required for any extensions)
-		auto vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, "vkCreateDebugReportCallbackEXT");
-		IM_ASSERT(vkCreateDebugReportCallbackEXT != NULL);
-
-		// Setup the debug report callback
-		VkDebugReportCallbackCreateInfoEXT debug_report_ci = {};
-		debug_report_ci.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
-		debug_report_ci.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT | VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT;
-		debug_report_ci.pfnCallback = debug_report;
-		debug_report_ci.pUserData = NULL;
-		err = vkCreateDebugReportCallbackEXT(g_Instance, &debug_report_ci, g_Allocator, &g_DebugReport);
-		check_vk_result(err);
-#else
-		// Create Vulkan Instance without any debug feature
-		err = vkCreateInstance(&create_info, g_Allocator, &g_Instance);
-		check_vk_result(err);
-		IM_UNUSED(g_DebugReport);
-#endif
-	}
+	g_Instance = vk::createInstance(instance_create_info);
 
 	// Select GPU
 	{
-		uint32_t gpu_count;
-		err = vkEnumeratePhysicalDevices(g_Instance, &gpu_count, NULL);
-		check_vk_result(err);
-		IM_ASSERT(gpu_count > 0);
-
-		VkPhysicalDevice* gpus = (VkPhysicalDevice*)malloc(sizeof(VkPhysicalDevice) * gpu_count);
-		err = vkEnumeratePhysicalDevices(g_Instance, &gpu_count, gpus);
-		check_vk_result(err);
-
-		// If a number >1 of GPUs got reported, find discrete GPU if present, or use first one available. This covers
-		// most common cases (multi-gpu/integrated+dedicated graphics). Handling more complicated setups (multiple
-		// dedicated GPUs) is out of scope of this sample.
+		auto gpus = g_Instance.enumeratePhysicalDevices();
 		int use_gpu = 0;
-		for (int i = 0; i < (int)gpu_count; i++)
+		for (int i = 0; i < (int)gpus.size(); i++)
 		{
-			VkPhysicalDeviceProperties properties;
-			vkGetPhysicalDeviceProperties(gpus[i], &properties);
-			if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+			auto properties = gpus[i].getProperties();			
+			if (properties.deviceType == vk::PhysicalDeviceType::eDiscreteGpu)
 			{
 				use_gpu = i;
 				break;
 			}
 		}
 
-		g_PhysicalDevice = gpus[use_gpu];
-		free(gpus);
+		g_PhysicalDevice = gpus.at(use_gpu);
 	}
 
 	// Select graphics queue family
 	{
-		uint32_t count;
-		vkGetPhysicalDeviceQueueFamilyProperties(g_PhysicalDevice, &count, NULL);
-		VkQueueFamilyProperties* queues = (VkQueueFamilyProperties*)malloc(sizeof(VkQueueFamilyProperties) * count);
-		vkGetPhysicalDeviceQueueFamilyProperties(g_PhysicalDevice, &count, queues);
-		for (uint32_t i = 0; i < count; i++)
-			if (queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		auto queues = g_PhysicalDevice.getQueueFamilyProperties();
+
+		for (uint32_t i = 0; i < queues.size(); i++)
+			if (queues[i].queueFlags & vk::QueueFlagBits::eGraphics)
 			{
 				g_QueueFamily = i;
 				break;
 			}
-		free(queues);
+	
 		IM_ASSERT(g_QueueFamily != (uint32_t)-1);
 	}
 
 	{
-		auto device_extensions = { "VK_KHR_swapchain" };
-		const float queue_priority = 1.0f;
+		auto extensions = { 
+			VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+		};
+
+		auto queue_priority = { 1.0f };
 
 		auto device_queue_create_info = vk::DeviceQueueCreateInfo()
 			.setQueueFamilyIndex(g_QueueFamily)
-			.setQueueCount(1)
-			.setPQueuePriorities(&queue_priority);
+			.setQueuePriorities(queue_priority);
 
 		auto device_create_info = vk::DeviceCreateInfo()
 			.setQueueCreateInfoCount(1)
 			.setPQueueCreateInfos(&device_queue_create_info)
-			.setPEnabledExtensionNames(device_extensions);
+			.setPEnabledExtensionNames(extensions);
 		
 		g_Device = g_PhysicalDevice.createDevice(device_create_info);
 		g_Queue = g_Device.getQueue(g_QueueFamily, 0);
@@ -239,13 +179,6 @@ static void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface
 static void CleanupVulkan()
 {
 	vkDestroyDescriptorPool(g_Device, g_DescriptorPool, g_Allocator);
-
-#ifdef IMGUI_VULKAN_DEBUG_REPORT
-	// Remove the debug report callback
-	auto vkDestroyDebugReportCallbackEXT = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, "vkDestroyDebugReportCallbackEXT");
-	vkDestroyDebugReportCallbackEXT(g_Instance, g_DebugReport, g_Allocator);
-#endif // IMGUI_VULKAN_DEBUG_REPORT
-
 	vkDestroyDevice(g_Device, g_Allocator);
 	vkDestroyInstance(g_Instance, g_Allocator);
 }
